530b3066b485fc8739d22b2e2ea7b47a5738bde2
AP <aleks.723@gmail.com>
2026-02-19 21:37:40 +0100

    Fix date and time display. Now properly handles timezones.

--- mproj/sysmon/views.py
+++ mproj/sysmon/views.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime, timedelta
 from django.http import HttpRequest, JsonResponse
+from django.utils import timezone
 
 from .models import MemoryData, CpuData
 from .utils import get_cpu_info, get_memory_info, get_system_time, get_uptime
@@ -16,19 +17,19 @@ def uptime(request: HttpRequest) -> JsonResponse:
 
 def memory(request: HttpRequest) -> JsonResponse:
     stats: dict[str, str | float | dict[datetime, str]] = get_memory_info()
-    historical_data = MemoryData.objects.filter(timestamp__gt=datetime.now()-timedelta(minutes=1))
+    historical_data = MemoryData.objects.filter(timestamp__gt=timezone.now()-timedelta(minutes=1))
     historical_data_list = {}
     for item in historical_data:
-        historical_data_list[item.timestamp.strftime("%Y-%m-%d %H:%M:%S")] = item.free
+        historical_data_list[item.timestamp.isoformat()] = item.free
 
-    s: MemoryData = MemoryData(timestamp=datetime.now(), free=stats['free_memory'], total=stats["total_memory"])
+    s: MemoryData = MemoryData(timestamp=timezone.now(), free=stats['free_memory'], total=stats["total_memory"])
     s.save()
     stats['history'] = historical_data_list
     return JsonResponse(stats)
 
 def cpu(request: HttpRequest) -> JsonResponse:
     stats: dict[str, str] = get_cpu_info()
-    s = CpuData(timestamp=datetime.now(), avg_load=stats["avg_load"])
+    s = CpuData(timestamp=timezone.now(), avg_load=stats["avg_load"])
     s.save()
     return JsonResponse(stats)
 
